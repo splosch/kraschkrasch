fc68082aa4ac579b21029598fff0e4dc351067c3
splosch <sploscho@googlemail.com>
2014-06-18 20:46:13 +0200

    Codecleanup Tabs in Leerzeichen und anpassen der Zeileneinrückung

--- js/analytics.js
+++ js/analytics.js
@@ -1,9 +1,15 @@
-﻿ var _gaq = _gaq || [];
- _gaq.push(['_setAccount', 'UA-29423983-1']);
- _gaq.push(['_trackPageview']);
+var _gaq = _gaq || [];
 
- (function() {
-   var ga = document.createElement('script'); ga.type = 'text/javascript'; ga.async = true;
-   ga.src = ('https:' == document.location.protocol ? 'https://ssl' : 'http://www') + '.google-analytics.com/ga.js';
-   var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(ga, s);
- })();
+_gaq.push(['_setAccount', 'UA-29423983-1']);
+_gaq.push(['_trackPageview']);
+
+(function() {
+var ga = document.createElement('script'),
+s  = document.getElementsByTagName('script')[0];
+
+ga.type  = 'text/javascript';
+ga.async = true;
+ga.src   = ('https:' == document.location.protocol ? 'https://ssl' : 'http://www') + '.google-analytics.com/ga.js';
+
+s.parentNode.insertBefore(ga, s);
+})();
--- js/myCommonJavaScript.js
+++ js/myCommonJavaScript.js
@@ -1,97 +1,101 @@
-function click (e) {
-  if (!e)
-    e = window.event;
-  if ((e.type && e.type == "contextmenu") || (e.button && e.button == 2) || (e.which && e.which == 3)) {
-    if (window.opera)
+function click (event) {
+  if (!event)
+  event = window.event;
+  if ( (event.type && event.type == "contextmenu") 
+       || (event.button && event.button == 2) 
+       || (event.which && e.which == 3)) {
+    
+    if (window.opera) {
       window.alert("Sorry: Diese Funktion ist deaktiviert.");
+    }
+
     return false;
   }
 }
 
 
-function maxOutImage(){
-    var newWidth = $(".content").width(),
-        maxHeight = $(".content").height(),
-        oldWidth = $(".fullscale").width(),
-        oldHeight = $(".fullscale").height(),
-        factor = newWidth / oldWidth,
-        newHeight = oldHeight * factor;
+function maxOutImage() {
+  var newWidth  = $(".content").width(),
+      maxHeight = $(".content").height(),
+      oldWidth  = $(".fullscale").width(),
+      oldHeight = $(".fullscale").height(),
+      factor    = newWidth / oldWidth,
+      newHeight = oldHeight * factor;
 
-    if(newHeight > maxHeight){
-        newHeight = maxHeight;
-        factor = newHeight / oldHeight;
-        newWidth = oldWidth * factor;
-    }
+  if(newHeight > maxHeight) {
+    newHeight = maxHeight;
+    factor    = newHeight / oldHeight;
+    newWidth  = oldWidth * factor;
+  }
 
-    // max size cap
-    if(newWidth > 920) {
-        newWidth = 920;
-        newHeight = oldHeight * (newWidth / oldWidth);
-    }
+  // max size cap
+  if(newWidth > 920) {
+    newWidth  = 920;
+    newHeight = oldHeight * (newWidth / oldWidth);
+  }
 
-    $("article, .fullscale").width(newWidth)
-                            .height(newHeight);
-
+  $("article, .fullscale").width(newWidth)
+                          .height(newHeight);
 
 // force slider to update size
-    $(window).trigger("load");
+  $(window).trigger("load");
 }
 
 
-$(document).ready(function(){
-    $(".fullscale").eq(0).bind("load", maxOutImage);
+$(document).ready(function() {
+  $(".fullscale").eq(0).bind("load", maxOutImage);
+  $(".startbild").eq(0).bind("load", function(){ $(this).fadeIn(); });
 
+  $(".details").bind("click", function() {
+    $(".detailsWrapper").toggleClass("collapsed");
+  });
 
-    $(".startbild").eq(0).bind("load", function(){$(this).fadeIn();});
+  $('.autoExpand img').eq(0).load(
+  function() {
+    window.setTimeout(function(){$('#detailsBox').toggleClass('collapsed',false)}, 500);
+  }
+  );
 
-    $(".details").bind("click", function(){
-        $(".detailsWrapper").toggleClass("collapsed");
-    });
+  $(window).bind("resize", function() {
+    maxOutImage();
+  });
 
-    $('.autoExpand img').eq(0).load(
-    function() {
-      window.setTimeout(function(){$('#detailsBox').toggleClass('collapsed',false)}, 500);
+  var options = typeof sliderOptions === "object" ? sliderOptions : {
+    width: 900,
+    height: 600,
+
+    play: {
+      active: false,
+      // [boolean] Generate the play and stop buttons.
+      // You cannot use your own buttons. Sorry.
+      effect: "slide",
+      // [string] Can be either "slide" or "fade".
+      interval: 5000,
+      // [number] Time spent on each slide in milliseconds.
+      auto: true,
+      // [boolean] Start playing the slideshow on load.
+      pauseOnHover: false,
+      // [boolean] pause a playing slideshow on hover
+      restartDelay: 2500
+      // [number] restart delay on inactive slideshow
+    },
+    slide: {       // Slide effect settings.
+      speed: 4000 // [number] Speed in milliseconds of the slide animation.
+    },
+    pagination: {
+      active: true,
+      effect: "slide" //"slide" or "fade".
     }
-    );
+  };
 
-    $(window).bind("resize", function() {
-        maxOutImage();
-    });
-
-    var options = typeof sliderOptions === "object" ? sliderOptions : {
-        width: 900,
-        height: 600,
-
-        play: {
-            active: false,
-            // [boolean] Generate the play and stop buttons.
-            // You cannot use your own buttons. Sorry.
-            effect: "slide",
-            // [string] Can be either "slide" or "fade".
-            interval: 5000,
-            // [number] Time spent on each slide in milliseconds.
-            auto: true,
-            // [boolean] Start playing the slideshow on load.
-            pauseOnHover: false,
-            // [boolean] pause a playing slideshow on hover
-            restartDelay: 2500
-            // [number] restart delay on inactive slideshow
-        },
-        slide: {       // Slide effect settings.
-            speed: 4000 // [number] Speed in milliseconds of the slide animation.
-        },
-        pagination: {
-            active: true,
-            effect: "slide" //"slide" or "fade".
-        }
-    };
-
-    if($('#slider')[0]){
-        $("#slider").slidesjs(options);
-    }
+  if($('#slider')[0]) {
+    $("#slider").slidesjs(options);
+  }
 });
 
-if (document.layers)
-    document.captureEvents(Event.MOUSEDOWN);
-document.onmousedown = click;
+if (document.layers) {
+  document.captureEvents(Event.MOUSEDOWN);
+}
+
+document.onmousedown   = click;
 document.oncontextmenu = click;
