ec92102d999f50e8e0bea652ee5afe279bad855c
splosch <sploscho@googlemail.com>
2014-08-28 19:19:41 +0200

    [WIP] set the current navigation section active

--- js/application.js
+++ js/application.js
@@ -2,13 +2,18 @@
  * Configuration
  * TODO: move to seperate file
  */
-var sections = {
-      home : {url : "#/"},
-      what : {url : "#/what"},
-      who  : {url : "#/who"},
-      when : {url : "#/when"},
-      where: {url : "#/where"}
-    };
+var  NAV_SECTIONS = [
+      { name: "home",  url: "#/"      },
+      { name: "what",  url: "#/what"  },
+      { name: "who",   url: "#/who"   },
+      { name: "when",  url: "#/when"  },
+      { name: "where", url: "#/where" }
+    ],
+
+    sections_source   = $("#hb_nav_sections").html(),
+    product_source    = $("#hb_product").html(),
+    sections_template = Handlebars.compile(sections_source),
+    product_template  = Handlebars.compile(product_source);
 
 /* configuration end */
 
@@ -21,23 +26,40 @@ var sections = {
  */
 var app = $.sammy(function( ) {
 
-  this.setSection = function(section){
-    if ( !!sections[section] ) {
-      this.section = section;
+  this.navSections = {}
 
-      console.log("switched to section '"+section+"'");
+  // TODO: Move into initialize method
+  // prime the sections once with NAV_SECTIONS data for later (fast) lookup
+  for (var i = 0, len = NAV_SECTIONS.length; i < len; i++) {
+    this.navSections[NAV_SECTIONS[i].name] = NAV_SECTIONS[i];
+  }
+
+  // updates the Navigation Section when a change occurs
+  this.setNavSection = function(section){
+    var sectionData = {};
+
+    sectionData.NAV_SECTIONS  = NAV_SECTIONS;
+
+    debugger;
+    if ( section && this.navSections[section]) {
+      // keep to check wheter update of nav_section is needed
+      this.activeSection = sectionData.activeSection = section;
+
+      $("#nav_sections").html(sections_template(sectionData));
+
+      console.log("switched to section '"+this.activeSection+"'");
     } else {
-      console.log("invalid section '"+section+"'");
+      console.log("invalid section '"+this.activeSection+"'");
     }
   };
 
   // include the plugin and alias handlebars() to hb()
   this.use('Handlebars', 'hb');
-  this.section = "home";
+  this.activeSection = "home";
 
   // defining the basic routes
   this.get('#/',      function() { 
-    this.app.setSection("home");
+    this.app.setNavSection("home");
     this.render('hb_nav_sections.hb', null, { hb_slider: 'hb-partial_slider.hb' });
 
     /* TODO Make this work 
@@ -49,13 +71,13 @@ var app = $.sammy(function( ) {
     /  );
     */
   });
-  this.get('#/what',  function() { this.app.setSection("what"); });
-  this.get('#/who',   function() { this.app.setSection("who"); });
-  this.get('#/when',  function() { this.app.setSection("when"); });
-  this.get('#/where', function() { this.app.setSection("where"); });
+  this.get('#/what',  function() { this.app.setNavSection("what"); });
+  this.get('#/who',   function() { this.app.setNavSection("who"); });
+  this.get('#/when',  function() { this.app.setNavSection("when"); });
+  this.get('#/where', function() { this.app.setNavSection("where"); });
 
   this.get('#/what/product/:productname', function() {
-    this.app.setSection("what");
+    this.app.setNavSection("what");
     alert("Switched to product: " + this.params['productname'])
   })
 });
