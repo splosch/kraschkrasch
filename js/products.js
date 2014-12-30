a29fb7c88b0016d5bbc53d0c5fbc52061ed5aaad
splosch <sploscho@googlemail.com>
2014-12-30 09:20:50 +0100

    Transfer whatDetailPages into product configuration data

--- js/products.js
+++ js/products.js
@@ -2,7 +2,7 @@
  * {
  *   @"name"        e.g. "bulla"           Links und Ordner"name", kleingeschrieben
  *   @"title":      e.g. "Bulb"            Echter Produkt"name", Sonderzeichen, Gross/Klein-Schreibung, Umlaute
- *   @"section":    e.g. "furniture"       Navigationsbereich (furniture, lightning, products)
+ *   @"section":    e.g. "furniture"       Navigationsbereich (furniture, lighting, products)
  *   @"material":   e.g. "glass | metal"   Verwendete Materialien getrennt durch ein "|"
  *   @"dimensions": e.g. "90 | 120 | 160"  Abmessungen
  *   @"development": {                     Zusatzinfos
@@ -58,7 +58,7 @@ var products = [
   },
   {
     "name":       "fju",
-    "title":      "fju",
+    "title":      "Fju",
     "section":    "furniture",
     "material":   "wood",
     "dimensions": "900 | 450 | 130",
@@ -66,20 +66,20 @@ var products = [
     "description": "Is a wallmounted and foldable desk. When folded down it serves as a small workspace. Folded up Fju changes it‘s appearence and transforms into a spacesaving storage shelf. You just put your stuff (laptop, ipad and books) in the case/ shelf underneath the desktop - et voilà.",
     "producers": [
       {
-        "name": "_free to edition",
+        "name": "_comming soon",
         "url": null
       }
     ],
     "images": [1,2,3,4,5,6]
   },
   {
-    "name":       "keep",
-    "title":      "keep",
-    "section":    "lightning",
-    "material":   "metal",
-    "dimensions": "280 | 95",
-    "development": { "purpose": "wall lamp", "year" : "2014" },
-    "description": "The idea of „keep“ is to combine a wall lamp with storage space. The shell-like shaped lamp offers the possibility to store little things like keys, glasses, etc. The light source is located behind the upper semicircle and scatters its light into the whole volume of the lamp – and its surroundings.",
+    "name":       "raft",
+    "title":      "Raft",
+    "section":    "furniture",
+    "material":   "wood | metal",
+    "dimensions": "1000 | 260 | 35",
+    "development": { "purpose": "shelf", "year" : "2014" },
+    "description": "The inspiration for raft - as the name suggests - is a wooden raft with its round timber. Due to its round shape, the shelf has a fascinating haptics or rather look. The bookend can be perfectly connected to the shape of the round timber.",
     "producers": [
       {
         "name": "_free to edition",
@@ -89,9 +89,188 @@ var products = [
     "images": [1,2,3,4,5,6]
   },
   {
+    "name":       "plank",
+    "title":      "Hide&Park",
+    "section":    "furniture",
+    "material":   "wood",
+    "dimensions": "450 | 1000 | 1600",
+    "development": { "purpose": "swardrobe, storage", "year" : "2014" },
+    "description": "Hide&Park is a wardrobe with a very simple look but a lot of functionality. It shows the pure beauty of the wooden material in a very minimalistic way. As common for a wardrobe you can hang up your clothes, but Hide&Park also leaves space to store your daily stuff (wallet, phone, keys...) on top of it.",
+    "producers": [
+      {
+        "name": "ZEITRAUM",
+        "url": "http://www.zeitraum-moebel.de/kollektion/hide-park"
+      }
+    ],
+    "images": [1,2,3,4,5,6],
+    "extras": {
+      "video" : "https://www.youtube.com/watch?v=35WNFOKZh0s"
+    }
+  },
+  {
+    "name":       "check",
+    "title":      "Check",
+    "section":    "furniture",
+    "material":   "metal | mirror",
+    "dimensions": "1800 | 415",
+    "development": { "purpose": "floor mirror", "year" : "2013" },
+    "description": "is a wardrobe/ mirror which leans against the wall. The triangle on the top allows to hang clothes up, or to throw them on top. Due to the possibilty of leaning it into different directions, its function is always defined by the user.",
+    "producers": [
+      {
+        "name": "MENU",
+        "url": "http://menu.as/"
+      }
+    ],
+    "images": [1,2,3,4,5,6]
+  },
+  {
+    "name":       "scoop_table",
+    "title":      "Scoop",
+    "section":    "furniture",
+    "material":   "wood",
+    "dimensions": "740 | 800 | 800",
+    "development": { "purpose": "table", "year" : "2013" },
+    "description": "combines traditional handcraft with industrial process. Its simple construction provides the opportunity to play with dimension. The table-legs are inspired by the shape of wooden scoops.",
+    "producers": [
+      {
+        "name": "_free to edition",
+        "url": null
+      }
+    ],
+    "images": [1,2,3,4,5,6]
+  },
+  {
+    "name":       "scoop_chair",
+    "title":      "Scoop",
+    "section":    "furniture",
+    "material":   "wood",
+    "dimensions": "800 | 440 | 400",
+    "development": { "purpose": "chair", "year" : "2013" },
+    "description": "combines traditional craft with industrial process. The chair-legs are inspired by the shape of wooden scoops. Its straight shape gives it an elegant appearance and makes it versatile.",
+    "producers": [
+      {
+        "name": "_free to edition",
+        "url": null
+      }
+    ],
+    "images": [1,2,3,4,5,6]
+  },
+  {
+    "name":       "anna_lena",
+    "title":      "Bubka",
+    "section":    "furniture",
+    "material":   "metal",
+    "dimensions": "2000 | 30",
+    "development": { "purpose": "coatrack", "year" : "2011" },
+    "description": "is a wardrobe which leans against the wall. It seems the pipes, which are made of aluminium, cross in one point. Through that joint the wardrobe can be dismantled. The minimalist design has a graphic presentation.",
+    "producers": [
+      {
+        "name": "MAGAZIN",
+        "url": "http://www.magazin.com/garderobe-bubka-p1463692/?c=194744"
+      }
+    ],
+    "images": [1,2,3,4,5,6]
+  },
+  {
+    "name":       "pinokkio",
+    "title":      "H&ouml;ninger",
+    "section":    "furniture",
+    "material":   "wood | metal",
+    "dimensions": "2000 | 730 | 680<br>2000 | 430 | 300",
+    "development": { "purpose": "table bench", "year" : "2011" },
+    "description": "The table-/bench-set is made of solid wood. The legs are fixed through anodized joints. Inspiration for this piece of furniture comes from the scandinavian design, especially by the 50`s and 60`s.",
+    "producers": [
+      {
+        "name": "MAGAZIN",
+        "url": "http://www.magazin.com/tisch-bank-hoeninger-p1462880/?a=42288"
+      }
+    ],
+    "images": ["pinokkio_01.jpg","pinokkio_02.jpg","pinokkio_03.jpg","pinokkio_04.jpg","pinokkio_05.jpg","pinokkio_06.jpg"]
+  },
+  {
+    "name":       "pina_side_table",
+    "title":      "Pina",
+    "section":    "furniture",
+    "material":   "wood",
+    "dimensions": "430 | 400",
+    "development": { "purpose": "side table", "year" : "2011" },
+    "description": "is characterized by its material and its certain appearance. Modern craftmanship gives the side table its special character. The color stained surface adds what is needed for a young and fresh look.",
+    "producers": [
+      {
+        "name": "HUBERT FELDKIRCHER",
+        "url": "http://www.hubert-feldkircher.at/cms/index.php?option=com_content&view=article&id=29&Itemid=62&lang=de"
+      }
+    ],
+    "images": ["pina_side_table_01.jpg","pina_side_table_02.jpg","pina_side_table_03.jpg","pina_side_table_04.jpg","pina_side_table_05.jpg","pina_side_table_06.jpg"]
+  },
+  {
+    "name":       "tome",
+    "title":      "Tome",
+    "section":    "furniture",
+    "material":   "wood",
+    "dimensions": "800 | 590 | 500",
+    "development": { "purpose": "chair", "year" : "2012" },
+    "description": "is a chair with a high-quality crafted character. Due to its soft transitions Tome reminds with its backrest on the characteristic of plastic chairs. Tome combines aesthetics with seating comfort. The chair is inspired by scandinavian design, especially from the 50 ́s and 60 ́s.",
+    "producers": [
+      {
+        "name": "_free to edition",
+        "url": null
+      }
+    ],
+    "images": ["tome_chair_01.jpg","tome_chair_02.jpg","tome_chair_03.jpg","tome_chair_04.jpg","tome_chair_05.jpg","tome_chair_06.jpg"]
+  },
+  {
+    "name":       "tamp_lable",
+    "title":      "Tamp&Lable",
+    "section":    "furniture",
+    "material":   "wood | composite panel",
+    "dimensions": "1600 | 760 | 750",
+    "development": { "purpose": "writing desk", "year" : "2010" },
+    "description": "is a lamp and a table in one fitment. This furniture combines a writing desk with a desk lamp. The pull switch is a fifties excerp. The lamp is 360° pivotable and you can plug in the light arm on both sides.",
+    "producers": [
+      {
+        "name": "_free to edition",
+        "url": null
+      }
+    ],
+    "images": ["tamp_lable_01.jpg","tamp_lable_02.jpg","tamp_lable_03.jpg","tamp_lable_04.jpg","tamp_lable_05.jpg","tamp_lable_06.jpg"]
+  },
+  {
+    "name":       "cherrytable",
+    "title":      "Cherry",
+    "section":    "furniture",
+    "material":   "wood | magnet | metal",
+    "dimensions": "400 | 150",
+    "development": { "purpose": "table lamp", "year" : "2014" },
+    "description": "The cherry table lamp has a clean and simple look along with a metal cylinder base. The lamp features a hidden magnet allowing the wood cylinder to grab the metal base easily.",
+    "producers": [
+      {
+        "name": "ESAILA",
+        "url": "http://www.esaila.com/cherry-table-lamp"
+      }
+    ],
+    "images": [1,2,3,4,5,6]
+  },
+  {
+    "name":       "keep",
+    "title":      "Keep",
+    "section":    "lighting",
+    "material":   "metal",
+    "dimensions": "280 | 95",
+    "development": { "purpose": "wall lamp", "year" : "2014" },
+    "description": "The idea of „keep“ is to combine a wall lamp with storage space. The shell-like shaped lamp offers the possibility to store little things like keys, glasses, etc. The light source is located behind the upper semicircle and scatters its light into the whole volume of the lamp – and its surroundings.",
+    "producers": [
+      {
+        "name": "_comming soon",
+        "url": null
+      }
+    ],
+    "images": ["keep_01.jpg","keep_02.jpg","keep_03.jpg","keep_04.jpg","keep_05.jpg","keep_06.jpg"]
+  },
+  {
     "name":       "cherry",
     "title":      "Cherry",
-    "section":    "lightning",
+    "section":    "lighting",
     "material":   "wood | magnet",
     "dimensions": "220 | 95",
     "development": { "purpose": "pendant lamp", "year"   : "2013" },
@@ -108,9 +287,111 @@ var products = [
     }
   },
   {
+    "name":       "konichiwa",
+    "title":      "Konichiwa",
+    "section":    "lighting",
+    "material":   "metal",
+    "dimensions": "231 | 162 | 70",
+    "development": { "purpose": "table lamp", "year": "2014" },
+    "description": "is a geometrical table lamp with a straight forward and almost simple look. A solid steel shaft has been deformed through heating. The powercable goes through the kink and all the way down to the bottom.",
+    "producers": [
+      {
+        "name": "_free to edition",
+        "url": null
+      }
+    ],
+    "images": [1,2,3,4,5,6],
+    "extras": {
+      "video": "http://www.youtube.com/watch?v=K8jav3ViIGA"
+    }
+  },
+  {
+    "name":       "industrial",
+    "title":      "Industrial",
+    "section":    "lighting",
+    "material":   "glass | wood",
+    "dimensions": "145 | 260 | 360",
+    "development": { "purpose": "pendant lamp", "year": "2013" },
+    "description": "is a lamp family inspired by the shape of old industrial lamps. They are made out of hand blown glass. The shades are carried by a wooden cone. The different types of lamp can be combined with each other.",
+    "producers": [
+      {
+        "name": "DREIZEHNGRAD",
+        "url": "http://www.dreizehngrad.de/pages-de/index.php"
+      }
+    ],
+    "images": [1,2,3,4,5,6],
+    "extras": {
+      "notice": "Interior Innovation Award 2014"
+    }
+  },
+  {
+    "name":       "kalimero",
+    "title":      "Cap",
+    "section":    "lighting",
+    "material":   "magnet",
+    "dimensions": "1450 | 500 | 360",
+    "development": { "purpose": "table lamp", "year": "2012" },
+    "description": "is a floor and a table lamp. The lampshade is fixed on the globe through a spring steel clip, so it is possible to pan it in nearly every direction. The lamp consists of 3 parts... the lamp stand, the lampshade and the tube with bulb and fitting – Visually so simple and highly-technical at the same time...",
+    "producers": [
+      {
+        "name": "NORMANN COPENHAGEN",
+        "url": "http://www.normann-copenhagen.com/products/cap-tablelamp?v=505043"
+      }
+    ],
+    "images": ["kalimero_01.jpg","kalimero_02.jpg","kalimero_03.jpg","kalimero_04.jpg","kalimero_05.jpg","kalimero_06.jpg"]
+  },
+  {
+    "name":       "pina_table_lamp",
+    "title":      "Pina",
+    "section":    "lighting",
+    "material":   "wood | metal",
+    "dimensions": "420 | 400",
+    "development": { "purpose": "table lamp", "year": "2012" },
+    "description": "is a dimmable lamp. The light just shines through a small ring. The combination of a light foot and a heavy lamp shade is both, elegant and eye catching.",
+    "producers": [
+      {
+        "name": "_comming soon",
+        "url": null
+      }
+    ],
+    "images": ["pina_table_lamp_01.jpg","pina_table_lamp_02.jpg","pina_table_lamp_03.jpg","pina_table_lamp_04.jpg","pina_table_lamp_05.jpg","pina_table_lamp_06.jpg"]
+  },
+  {
+    "name":       "flachmann",
+    "title":      "Flachmann",
+    "section":    "lighting",
+    "material":   "metal",
+    "dimensions": "430",
+    "development": { "purpose": "pendant lamp", "year": "2011" },
+    "description": "is a pendant lamp which impresses through its simplicity. All it takes is a lampshade a lampholder and a bulb - that‘s it.",
+    "producers": [
+      {
+        "name": "T&Oslash;NDEL",
+        "url": "http://toendel.de/index.php?option=com_virtuemart&page=shop.browse&category_id=2&Itemid=63"
+      }
+    ],
+    "images": ["flachmann_01.jpg","flachmann_02.jpg","flachmann_03.jpg","flachmann_04.jpg","flachmann_05.jpg","flachmann_06.jpg"]
+  },
+  {
+    "name":       "candleblocks",
+    "title":      "Candleblocks",
+    "section":    "products",
+    "material":   "ceramic",
+    "dimensions": "85 | 110 | 85",
+    "development": { "purpose": "candleholder", "year" : "2014" },
+    "description": "The candleblocks are playful and fun objects. Due to distinct but related shapes, they can be arranged and decorated to an unlimited extent. Matching colours have been chosen to offer countless opportunities for your decorating ideas!",
+    "producers": [
+      {
+        "name": "BOLIA",
+        "url": "http://www.bolia.com/en-us/designers/kaschkasch/24-459-01_5470862"
+      }
+    ],
+    "images": [1,2,3,4,5,6]
+  },
+  {
     "name":       "luna",
-    "title":      "luna",
-    "section":    "lightning",
+    "title":      "Luna",
+    "section":    "products",
     "material":   "metal",
     "dimensions": "270 | 140",
     "development": { "purpose": "vase bowl", "year" : "2014" },
@@ -123,6 +404,57 @@ var products = [
     ],
     "images": [1,2,3,4,5,6]
   },
+  {
+    "name":       "bulla",
+    "title":      "Bulb",
+    "section":    "products",
+    "material":   "glass | metal",
+    "dimensions": "90 | 120 | 160",
+    "development": { "purpose": "coat hook", "year" : "2013" },
+    "description": "are handblown glass hooks. Different sizes and colours decorate the wall and help you organize.",
+    "producers": [
+      {
+        "name": "SCH&Ouml;NBUCH",
+        "url": "http://www.schoenbuch.com/de/wohnen/interior-accessoires/garderobenhaken/bulb.html"
+      }
+    ],
+    "images": [1,2,3,4,5,6]
+  },
+  {
+    "name":       "bonfire",
+    "title":      "Bonfire",
+    "section":    "products",
+    "material":   "silicone | magnet",
+    "dimensions": "150 | 15",
+    "development": { "purpose": "trivet", "year" : "2013" },
+    "description": "Is a playful trivet inspired by branches used for a bonfire. The two pieces connect due to integrated magnets. The angular outline ensures great stability for pans and pots. Using the trivet feels like sitting around a cosy bonfire.",
+    "producers": [
+      {
+        "name": "_free to edition",
+        "url": null
+      }
+    ],
+    "images": [1,2,3,4,5,6],
+    "extras": {
+      "video": "http://www.youtube.com/watch?v=cF6M45Q2Iqg&feature=youtu.be"
+    }
+  },
+  {
+    "name":       "hopper_boxes",
+    "title":      "Hopper",
+    "section":    "products",
+    "material":   "wood",
+    "dimensions": "220",
+    "development": { "purpose": "boxes", "year": "2012" },
+    "description": "are an accessory series of boxes consisting of stacked cylinders in different heights. The gently projecting cylinders are considering functional and aesthetically aspects. The project is inspired by the traditional japanese wappa bowl.",
+    "producers": [
+      {
+        "name": "_free to edition",
+        "url": null
+      }
+    ],
+    "images": ["hopper_boxes_01.jpg","hopper_boxes_02.jpg","hopper_boxes_03.jpg","hopper_boxes_04.jpg","hopper_boxes_05.jpg","hopper_boxes_06.jpg"]
+  }
 ];
 
 var products = (function(){
